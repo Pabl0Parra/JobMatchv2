24802e12cc7365028971e785ecaa038f3f834b1a
Pabl0Parra <pabsblue16@gmail.com>
2023-02-09 19:53:48 +0100

    refactor ChooseCountry

--- src/screens/ChooseCountry.jsx
+++ src/screens/ChooseCountry.jsx
@@ -1,64 +1,48 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import React from "react";
+// import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import InputForm from "../components/InputForm";
 
 const ChooseCountry = ({ navigation }) => {
-  const [country, setCountry] = useState("");
-  const [city, setCity] = useState("");
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeText}>Te damos la bienvenida,</Text>
-      <Text style={styles.requestText}>Introduce los siguientes datos:</Text>
-      <TextInput
-        style={styles.input}
-        value={country}
-        placeholder="País"
-        onChangeText={(text) => setCountry(text)}
-      />
-      <TextInput
-        style={styles.input}
-        value={city}
-        placeholder="Ciudad"
-        onChangeText={(text) => setCity(text)}
-      />
-      <Button
-        title="Siguiente"
-        onPress={() => navigation.navigate("CompanyName", { country, city })}
-        style={styles.nextButton}
-      />
-    </View>
+    <InputForm
+      fields={[
+        { label: "País", value: "" },
+        { label: "Ciudad", value: "" },
+      ]}
+      onSubmit={(values) => {
+        navigation.navigate("ChooseUserName", {
+          country: values[0],
+          city: values[1],
+        });
+      }}
+      questionText="Te damos la bienvenida,"
+      requestText="Introduce los siguientes datos:"
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "start",
-    padding: 32,
-    backgroundColor: "#fff",
-    marginTop: 80,
-  },
-  welcomeText: {
-    fontSize: 22,
-    marginBottom: 10,
-  },
-  requestText: {
-    fontSize: 16,
-    marginBottom: 16,
-  },
-  input: {
-    height: 40,
-    width: 300,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    marginVertical: 10,
-  },
-  nextButton: {
-    marginTop: 80,
-    width: "100%",
-    borderRadius: 200,
-  },
-});
+// <View style={styles.container}>
+//   <Text style={styles.welcomeText}>Te damos la bienvenida,</Text>
+//   <Text style={styles.requestText}>Introduce los siguientes datos:</Text>
+//   <TextInput
+//     style={styles.input}
+//     value={country}
+//     placeholder="País"
+//     onChangeText={(text) => setCountry(text)}
+//   />
+//   <TextInput
+//     style={styles.input}
+//     value={city}
+//     placeholder="Ciudad"
+//     onChangeText={(text) => setCity(text)}
+//   />
+//   <Button
+//     title="Siguiente"
+//     onPress={() => navigation.navigate("CompanyName", { country, city })}
+//     style={styles.nextButton}
+//   />
+// </View>
+//   );
+// };
 
 export default ChooseCountry;
