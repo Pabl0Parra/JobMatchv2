bda301b271c6211a3ecaecd3a999bc2fa6e1fcc1
Pabl0Parra <pabsblue16@gmail.com>
2023-02-09 19:59:17 +0100

    refactor ChooseRoleWanted

--- src/screens/ChooseRoleWanted.jsx
+++ src/screens/ChooseRoleWanted.jsx
@@ -1,61 +1,17 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import React from "react";
+import InputForm from "../components/InputForm";
 
-const RoleWantedScreen = ({ navigation }) => {
-  const [role, setRole] = useState("");
-
+const ChooseRoleWanted = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.questionText}>¿Qué perfil buscas?</Text>
-      <Text style={styles.requestText}>
-        Por favor, introduce el rol deseado:
-      </Text>
-      <TextInput
-        style={styles.input}
-        value={role}
-        placeholder="Perfil"
-        onChangeText={(text) => setRole(text)}
-      />
-
-      <Button
-        title="Siguiente"
-        // TODO: Uncomment this line and pass the role to the next screen WHICH IS?
-        // onPress={() => navigation.navigate("Email", { role })}
-        style={styles.nextButton}
-      />
-    </View>
+    <InputForm
+      fields={[{ label: "Perfil", value: "" }]}
+      onSubmit={(values) => {
+        navigation.navigate("ChooseCompanyLogo", { role: values[0] });
+      }}
+      questionText="¿Qué perfil buscas?"
+      requestText="Por favor, introduce el rol deseado:"
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "start",
-    padding: 32,
-    backgroundColor: "#fff",
-    marginTop: 80,
-  },
-  questionText: {
-    fontSize: 22,
-    marginBottom: 10,
-  },
-  requestText: {
-    fontSize: 16,
-    marginBottom: 16,
-  },
-  input: {
-    height: 40,
-    width: 300,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    marginVertical: 10,
-  },
-  nextButton: {
-    marginTop: 80,
-    width: "100%",
-    borderRadius: 200,
-  },
-});
-
-export default RoleWantedScreen;
+export default ChooseRoleWanted;
