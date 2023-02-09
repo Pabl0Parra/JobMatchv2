a444c6f859baa4903591beaea7fd59448ad45b33
niicodeer <nicoradin.job@gmail.com>
2023-02-09 20:54:39 -0300

    added navigation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,34 +1,32 @@
-
-import { View, Text, StyleSheet } from "react-native";
-import Constants from 'expo-constants';
+import { View, Text, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
 import Card from "../components/Card";
 import Header from "../components/Header";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Header screen="Home"/>
+      <Header screen="Home" />
       <View style={styles.textHeader}>
         <Text>Hola, usuario!</Text>
         <Text>Estas son algunas de las vanactes disponibles:</Text>
       </View>
-      <Card />
+      <Card navigation={navigation}/>
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
-    marginTop:Constants.statusBarHeight,
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "#ECF0F1",
   },
-  textHeader:{
-    width:"90%",
-    textAlign:"left",
-  }
+  textHeader: {
+    width: "90%",
+    textAlign: "left",
+  },
 });
 export default Home;
