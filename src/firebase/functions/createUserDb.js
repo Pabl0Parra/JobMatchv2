3a4b558cf5bbb444ac96492e8173e651a5539c6b
nicoseper89 <nicosepertino@hotmail.com>
2023-02-10 09:46:55 -0300

    changes to createUserDB functions and Firebase credentials

--- src/firebase/functions/createUserDb.js
+++ src/firebase/functions/createUserDb.js
@@ -3,9 +3,9 @@ import { collection, addDoc } from "firebase/firestore";
 
 export default createUser = async (data) => {
   try {
-    const collectionRef = collection(db, "user");
+    const collectionRef = collection(db, "users");
     const userId = await addDoc(collectionRef, data);
-    console.log(userId);
+    return userId;
   } catch (error) {
     console.log(error);
   }
