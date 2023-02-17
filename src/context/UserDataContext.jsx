c3dff8c11e57bc578464174d773b47c179b87913
Pabl0Parra <pabsblue16@gmail.com>
2023-02-17 21:53:51 +0100

    create UserDataContext

---
+++ src/context/UserDataContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+export const UserDataContext = createContext();
+
+export const UserDataContextProvider = ({ children }) => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    type: "",
+    userName: "",
+    userLastName: "",
+    userRole: "",
+    userLastRole: "",
+    userLastCompany: "",
+    company: "",
+    sector: "",
+    roleWanted: "",
+    country: "",
+    city: "",
+  });
+
+  return (
+    <UserDataContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserDataContext.Provider>
+  );
+};
