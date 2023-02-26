917ccaee4d86e09f4b110f0377e322660f68f2c5
niicodeer <nicoradin.job@gmail.com>
2023-02-25 21:24:33 -0300

    added function to get Time difference and updated params

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,11 +13,44 @@ import { UserLoginContex } from "../context/UserDataContext";
 import Background from "../svgs/card_background.png";
 import theme from "../theme";
 import ActionsButtons from "./ActionsButtons";
+import { getTimeLapsed } from "../utilities/utilities";
+import { collection, doc, getDoc } from "@firebase/firestore";
+import { db, mainCollection } from "../firebase/credentials";
 
 const { colors, text } = theme;
 
 const Card = ({ card }) => {
+  const { userData } = useContext(UserLoginContex);
   const navigation = useNavigation();
+  const [time, setTime] = useState();
+  const [image, setImage] = useState();
+
+  const getCompanyImage = async () => {
+    const docRef = doc(db, mainCollection, card.userId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { image } = docSnap.data();
+      setImage(image);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    if (userData.worker) {
+      const fireBaseTime = new Date(card.timestamp.seconds * 1000);
+      setTime(getTimeLapsed(fireBaseTime));
+      getCompanyImage();
+    } else {
+      card.image !== ""
+        ? setImage(card.image)
+        : setImage(
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+          );
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -25,7 +58,7 @@ const Card = ({ card }) => {
         <View style={styles.header}>
           <Image
             source={{
-              uri: card.image,
+              uri: image,
             }}
             style={styles.image}
           />
@@ -59,36 +92,157 @@ const Card = ({ card }) => {
           >
             <View style={styles.footer}>
               <ScrollView>
-              {/* //TODO: calcular hace cuanto fue publicado el empleo */}
-                <Text style={[text[12],{color:"#fff"}]}>Hace 5 horas</Text>
-                <Text style={[text.cardTitle,{color:"#fff"}]}>{card.vacant}</Text>
-                <Text style={[text[16],{color:`${colors.details}`, fontStyle:"italic"}]}>{card.seniority}</Text>
-                <View style={{marginVertical:16}}>
-                  <Text style={[text.cardSubtitleMedium,{color:"#fff"}]}>{card.name}</Text>
-                  {/* //TODO: colocar variable de pais y demás */}
-                  <Text style={[text[14],{color:"#fff"}]}>Argentina</Text>
-                  <Text style={[text[14],{color:"#fff"}]}>Jornada completa</Text>
-                  <Text></Text>
-                </View>
+                {userData.worker ? (
+                  <>
+                    <Text style={[text[12], { color: "#fff" }]}>
+                      Hace {time}
+                    </Text>
+                    <Text style={[text.cardTitle, { color: "#fff" }]}>
+                      {card.roleWanted}{" "}
+{/*                       <Text
+                        style={[
+                          text[16],
+                          { color: `${colors.primary}`, fontStyle: "normal" },
+                        ]}
+                      >
+                        {card.seniority}
+                      </Text> */}
+                    </Text>
+                    <Text
+                        style={[
+                          text[16],
+                          { color: `${colors.details}`, fontStyle: "italic" },
+                        ]}
+                      >
+                        {card.seniority}
+                      </Text>
+
+                    {/* Salario */}
+{/*                     <Text
+                      style={[
+                        text[14],
+                        { color: `${colors.details}`, fontStyle: "italic" },
+                      ]}
+                    >
+                      {card.salary}
+                    </Text> */}
+                    <View style={{ marginVertical: 16 }}>
+                      <Text
+                        style={[text.cardSubtitleMedium, { color: "#fff" }]}
+                      >
+                        {card.userName +
+                          (card.userLastName ? card.userLastName : "")}
+                      </Text>
+                      <Text style={[text[14], { color: "#fff" }]}>
+                        {card.country + (card.city ? ` - ${card.city}` : "")}
+                      </Text>
+                      <Text style={[text[14], { color: "#fff" }]}>
+                        {card.timeJob} - {card.mode}
+                        {"\n"}
+                      </Text>
+                    </View>
+                  </>
+                ) : (
+                  <>
+                    <Text>{"\n"}</Text>
+                    <Text style={[text.cardTitle, { color: "#fff" }]}>
+                      {card.userName +
+                        (card.userLastName ? card.userLastName : "")}
+                    </Text>
+                    <Text style={[text[16], { color: "#fff", fontSize: 18 }]}>
+                      {card.roleWanted}{" "}
+                      {card.seniority ? (
+                        <Text
+                          style={[
+                            text[14],
+                            { color: `${colors.details}`, fontStyle: "normal" },
+                          ]}
+                        >
+                          {"   "}
+                          {card.seniority}
+                        </Text>
+                      ) : null}
+                    </Text>
+
+                    <View style={{ marginVertical: 16 }}>
+                      <Text
+                        style={[text.cardSubtitleMedium, { color: "#fff" }]}
+                      >
+                        {card.country}{" "}
+                        {card.city ? (
+                          <Text
+                            style={[
+                              text[14],
+                              {
+                                color: `${colors.primary}`,
+                                fontStyle: "italic",
+                              },
+                            ]}
+                          >
+                            {"- "}{card.city}
+                          </Text>
+                        ) : (
+                          <Text></Text>
+                        )}
+                      </Text>
+
+                      {card.userLastRole ? <Text style={[text[14], { color: `${colors.primary}` }]}>
+                      {"\n"}
+                        Último trabajo:{"\n"}
+                        <Text style={[text[14], { color: `${colors.primary}`}]}>
+                          {"  "}{card.userLastRole} en {card.userLastCompany}
+                        </Text>
+
+                      </Text> : <Text></Text>}
+                    </View>
+                  </>
+                )}
               </ScrollView>
             </View>
-
             <Pressable
               onPress={() =>
-                navigation.navigate("Details", {
+                navigation.navigate("Details", userData.worker ? {
                   //Agregar los datos necesarios para mostrar en pantalla
-                  vacant: card.vacant,
-                  image: card.image,
+                  //Datos de empresa
+                  image: image,
+                  roleWanted: card.roleWanted,
+                  seniority: card.seniority,
+                  country: card.country,
+                  mode: card.mode,
+                  timeJob: card.timeJob,
+                  requirements: card.requirements,
+                  salary: card.salary,
+                  hourHand: card.hourHand,
+                  contract: card.contract,
+                  education: card.education,
+                  experience: card.experience,
+                  english: card.english,
+                  functions: card.functions,
+                } : {
+                  //Datos de perfil
+                  image:image, 
+                  roleWanted: card.roleWanted,
+                  seniority: card.seniority,
+                  country: card.country,
+                  education: card.education,
+                  experience: card.experience,
+                  english: card.english,
+                  name: card.userName,
+                  lastName: card.userLastName,
+                  about: card.about,
                 })
               }
             >
               <Text
                 style={[
                   styles.footerText,
-                  { color: `${colors.details}`, textDecorationLine: "underline" },
+                  {
+                    color: `${colors.details}`,
+                    textDecorationLine: "underline",
+                  },
                 ]}
               >
-                Ver más detalles
+                Leer más
               </Text>
             </Pressable>
           </View>
