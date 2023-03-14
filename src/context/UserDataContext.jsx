4af28c3c032644925657aebfa933900610ab8f63
Pabl0Parra <pabsblue16@gmail.com>
2023-03-14 22:46:14 +0100

    fix bug

--- src/context/UserDataContext.jsx
+++ src/context/UserDataContext.jsx
@@ -35,7 +35,8 @@ export const UserDataContextProvider = ({ children }) => {
   return (
     <UserDataContext.Provider
       value={useMemo(() => {
-        userData, setUserData;
+        userData;
+        setUserData;
       })}
     >
       {children}
